0c774d466ebb6c6cb642f538623c71028835446a
GustavoMendel <gustavo.mendel.br@gmail.com>
2019-12-16 11:41:47 -0300

    Create ex009.py

---
+++ ex009.py
@@ -0,0 +1,23 @@
+valor = int(input('Digite um valor: '))
+
+x1 = valor * 1
+x2 = valor * 2
+x3 = valor * 3
+x4 = valor * 4
+x5 = valor * 5
+x6 = valor * 6
+x7 = valor * 7
+x8 = valor * 8
+x9 = valor * 9
+x10 = valor * 10
+
+print('1 x {} = {}'.format(valor, x1))
+print('2 x {} = {}'.format(valor, x2))
+print('3 x {} = {}'.format(valor, x3))
+print('4 x {} = {}'.format(valor, x4))
+print('5 x {} = {}'.format(valor, x5))
+print('6 x {} = {}'.format(valor, x6))
+print('7 x {} = {}'.format(valor, x7))
+print('8 x {} = {}'.format(valor, x8))
+print('9 x {} = {}'.format(valor, x9))
+print('10 x {} = {}'.format(valor, x10))
