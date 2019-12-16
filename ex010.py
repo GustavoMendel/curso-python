ca56e00e5be3dcdbcb526f2566a50af5d6e30d7f
GustavoMendel <gustavo.mendel.br@gmail.com>
2019-12-16 11:43:54 -0300

    Create ex010.py

---
+++ ex010.py
@@ -0,0 +1,7 @@
+valor = float(input('Quantos reais você possui? '))
+
+resultado = valor * 4.08
+
+print('Você pode comprar {} doláres.'.format(resultado))
+# levando em consideração a cotação do dólar em 16 Dez 2019
+# 1 dolar americano = 4.08 reais brasileiros
