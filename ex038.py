65f5bc8b6a18365b250477c0fcc7ee1c1fbf7eed
GustavoMendel <gustavo.mendel.br@gmail.com>
2019-12-16 23:17:46 -0300

    Create ex038.py

---
+++ ex038.py
@@ -0,0 +1,9 @@
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+
+if num1 > num2:
+    print('O PRIMEIRO número é maior')
+elif num1 < num2:
+    print('O SEGUNDO número é maior')
+else:
+    print('Os números são IGUAIS')
